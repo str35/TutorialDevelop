7b30e6b3f7215698763235c4911aeddcf2b9d44e
str35 <s-oya@fores.jp>
2025-03-04 19:42:52 +0900

    lesson18chapter9コミット

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.techacademy.controller;
 
+import java.util.Set;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.PathVariable; // 追加
 import org.springframework.web.bind.annotation.PostMapping; // 追加
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.User; // 追加
 import com.techacademy.service.UserService;
@@ -45,4 +48,31 @@ public class UserController {
         return "redirect:/user/list";
     }
     // ----- 追加:ここまで -----
+
+    /** User更新画面を表示 */
+    @GetMapping("/update/{id}/")
+    public String getUser(@PathVariable("id") Integer id, Model model) {
+        // Modelに登録
+        model.addAttribute("user", service.getUser(id));
+        // User更新画面に遷移
+        return "user/update";
+    }
+
+    /** User更新処理 */
+    @PostMapping("/update/{id}/")
+    public String postUser(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
 }
--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,7 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional; // 追加
 
@@ -21,6 +22,12 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+
     // ----- 追加:ここから -----
     /** Userの登録を行なう */
     @Transactional
@@ -28,5 +35,14 @@ public class UserService {
         return userRepository.save(user);
     }
     // ----- 追加:ここまで -----
+
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+
 
 }
