028aef1939fe14d2ece24eaa560ac6f940a08244
str35 <s-oya@fores.jp>
2025-03-17 18:52:20 +0900

    課題のコミット

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -58,16 +58,24 @@ public class UserController {
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
+    public String getUser(@PathVariable("id") Integer id, Model model, User user) {
+        if(id==null) {
+            model.addAttribute("user", user);
+        }else {
+         // Modelに登録
+            model.addAttribute("user", service.getUser(id));
+        }
         // User更新画面に遷移
         return "user/update";
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@PathVariable("id") Integer id, @Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getUser(null, model, user);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
